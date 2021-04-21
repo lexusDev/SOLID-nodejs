062f0b8ca88b2850b8ec2c035a0a0c224f823c36
Alex Monteiro <alex.monteiro@digiage.com.br>
2021-04-21 11:36:39 -0300

    finishing implementation

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,13 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const isAdmin = this.usersRepository.findById(user_id);
+
+    if (!isAdmin.admin) throw new Error("Is not admin!");
+
+    const users = this.usersRepository.list();
+
+    return users;
   }
 }
 
